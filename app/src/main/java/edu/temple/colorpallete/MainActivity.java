76a9cb5063ea5cfc8134ad3c534050a767d547b1
Daniel Burke <54857959+Dan-Burke-P@users.noreply.github.com>
2020-10-06 22:14:36 -0400

    Added grid view

--- app/src/main/java/edu/temple/colorpallete/MainActivity.java
+++ app/src/main/java/edu/temple/colorpallete/MainActivity.java
@@ -2,13 +2,31 @@ package edu.temple.colorpallete;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ColorObject[] colors = {
+            new ColorObject("red", Color.RED),
+            new ColorObject("blue", Color.BLUE),
+            new ColorObject("green", Color.GREEN),
+            new ColorObject("black", Color.BLACK),
+            new ColorObject("cyan", Color.CYAN),
+            new ColorObject("gray", Color.GRAY),
+            new ColorObject("yellow", Color.YELLOW),
+            new ColorObject("light gray", Color.LTGRAY),
+            new ColorObject("magenta", Color.MAGENTA),
+            new ColorObject("dark gray", Color.DKGRAY)
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ColorAdapter colorAdapter = new ColorAdapter(colors, getApplicationContext());
+        GridView gridView = (GridView)findViewById(R.id.colorPallete);
+        gridView.setNumColumns(3);
+        gridView.setAdapter(colorAdapter);
     }
 }
