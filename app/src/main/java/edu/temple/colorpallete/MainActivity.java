339c6a0c6cf20f73c5df239d71080cc0d42680da
Daniel Burke <54857959+Dan-Burke-P@users.noreply.github.com>
2020-10-14 13:23:01 -0400

    Colors now use string objects very cool

--- app/src/main/java/edu/temple/colorpallete/MainActivity.java
+++ app/src/main/java/edu/temple/colorpallete/MainActivity.java
@@ -3,6 +3,7 @@ package edu.temple.colorpallete;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -10,23 +11,26 @@ import android.widget.GridView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    ColorObject[] colors = {
-            new ColorObject("red", Color.RED),
-            new ColorObject("blue", Color.BLUE),
-            new ColorObject("green", Color.GREEN),
-            new ColorObject("black", Color.BLACK),
-            new ColorObject("cyan", Color.CYAN),
-            new ColorObject("gray", Color.GRAY),
-            new ColorObject("yellow", Color.YELLOW),
-            new ColorObject("light gray", Color.LTGRAY),
-            new ColorObject("magenta", Color.MAGENTA),
-            new ColorObject("dark gray", Color.DKGRAY)
-    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Resources res = this.getResources();
 
+        ColorObject[] colors = {
+                new ColorObject(res.getString(R.string.red), Color.RED),
+                new ColorObject(res.getString(R.string.blue), Color.BLUE),
+                new ColorObject(res.getString(R.string.green), Color.GREEN),
+                new ColorObject(res.getString(R.string.black), Color.BLACK),
+                new ColorObject(res.getString(R.string.cyan), Color.CYAN),
+                new ColorObject(res.getString(R.string.gray), Color.GRAY),
+                new ColorObject(res.getString(R.string.yellow), Color.YELLOW),
+                new ColorObject(res.getString(R.string.light_gray), Color.LTGRAY),
+                new ColorObject(res.getString(R.string.magenta), Color.MAGENTA),
+                new ColorObject(res.getString(R.string.dark_gray), Color.DKGRAY)
+        };
         ColorAdapter colorAdapter = new ColorAdapter(colors, getApplicationContext());
         GridView gridView = (GridView)findViewById(R.id.colorPallete);
         gridView.setNumColumns(3);
