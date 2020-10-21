bfb602b44cbdc10370a2902a8e456eec603f6153
Daniel Burke <54857959+Dan-Burke-P@users.noreply.github.com>
2020-10-21 14:03:14 -0400

    Completed

--- app/src/main/java/edu/temple/colorpallete/CanvasFragment.java
+++ app/src/main/java/edu/temple/colorpallete/CanvasFragment.java
@@ -9,17 +9,23 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class CanvasFragment extends Fragment {
 
+    View v;
+    TextView t;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_canvas, container, false);
+        v = inflater.inflate(R.layout.fragment_canvas, container, false);
+        t = v.findViewById(R.id.ColorDisplay);
+        return v;
     }
 
     public void updateColor(ColorObject colorObject){
         Log.println(Log.ASSERT,"tag", colorObject.name);
+        t.setText(colorObject.name);
+        v.setBackgroundColor(colorObject.color);
     }
 }
